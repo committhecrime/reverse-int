8b578ecdbcd8b7646538285bd2a4151fd3ed1ea4
Artur Makutonin <makutonin@Arturs-MacBook-Air.local>
2023-08-23 13:39:19 +0300

    docs: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
+
+    if(n < 0){
+        let a = n * (-1);
+        let b = a.toString().split("");
+        let c = b.reverse();
+        let d = c.join('');
+        let res = +d;
+        return res;
+    }else {
+        let b = n.toString().split("");
+        let c = b.reverse();
+        let d = c.join('');
+        let res = +d;
+        return res;
+      }
+    }
   
-}
